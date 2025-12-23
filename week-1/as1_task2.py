72317422581a21c2e3a6dccac53a7b44ea515062
deluna05 <hanimusya@mail.ru>
2025-12-23 20:34:09 +0500

    cleanup and update week 1 task2

--- week-1/as1_task2.py
+++ week-1/as1_task2.py
@@ -2,16 +2,7 @@ s1 = int(input("Salary 1: "))
 s2 = int(input("Salary 2: "))
 s3 = int(input("Salary 3: "))
 
-mx = s1
-if s2 > mx:
-    mx = s2
-if s3 > mx:
-    mx = s3
+mx = max(s1, s2, s3)
+mn = min(s1, s2, s3)
 
-mn = s1
-if s2 < mn:
-    mn = s2
-if s3 < mn:
-    mn = s3
-
-print("difference =", mx - mn)
+print("Difference =", mx - mn)
